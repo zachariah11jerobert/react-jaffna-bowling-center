3fd5b1b187909ceb5544c1597f7be9aa5cdbc930
unknown <zachariah11jerobert@gmail.com>
2022-02-17 22:02:40 +0530

    style done

--- src/components/GamesItem/GamesItem.js
+++ src/components/GamesItem/GamesItem.js
@@ -3,7 +3,8 @@ import "./games-item.scss";
 const GamesItem = ({ image, info }) => {
   return (
     <div className="games-container">
-      <img src={image} />
+      <div className="overlay"></div>
+      <img src={image}  />
       <div className="game-info pr-10">
         <h3>{info}</h3>
       </div>
