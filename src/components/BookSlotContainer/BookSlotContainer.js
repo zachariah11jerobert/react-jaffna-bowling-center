23eeff5e570cda5b9b5ba929a4e14f2d946d1f16
unknown <zachariah11jerobert@gmail.com>
2022-02-21 13:05:59 +0530

    done changes

--- src/components/BookSlotContainer/BookSlotContainer.js
+++ src/components/BookSlotContainer/BookSlotContainer.js
@@ -3,6 +3,36 @@ import badminton from "../../assets/badminton.jpg";
 import { IconContext } from "react-icons";
 import * as FaIcons from "react-icons/fa";
 
+const slots = [
+  {bookStatus:'e',bookText:'Book Now'},
+  {bookStatus:'c',bookText:'Confirmed'},
+  {bookStatus:'ca',bookText:'Cancelled'},
+  {bookStatus:'r',bookText:'Reserved'},
+  {bookStatus:'e',bookText:'Book Now'},
+  {bookStatus:'e',bookText:'Book Now'},
+  {bookStatus:'e',bookText:'Book Now'},
+  {bookStatus:'e',bookText:'Book Now'},
+  {bookStatus:'e',bookText:'Book Now'},
+  {bookStatus:'e',bookText:'Book Now'},
+  {bookStatus:'e',bookText:'Book Now'},
+  {bookStatus:'e',bookText:'Book Now'},
+  {bookStatus:'e',bookText:'Book Now'},
+  {bookStatus:'e',bookText:'Book Now'},
+  {bookStatus:'e',bookText:'Book Now'},
+  {bookStatus:'e',bookText:'Book Now'},
+];
+
+function todayDate() {
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, "0");
+  let mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+  let yyyy = today.getFullYear();
+
+  today = dd + "-" + mm + "-" + yyyy;
+
+  return today;
+}
+
 function pad(num) {
   num = num.toString();
   while (num.length < 2) num = "0" + num;
@@ -27,10 +57,10 @@ const TimeSlots = () => {
 const Court = () => {
   return (
     <ul>
-      {[...Array(16)].map((e, i) => {
+      {slots.map((slot, i) => {
         return (
           <li key={i} className="bg-gray mt-2 ml-2 slot-container-court">
-            X
+            {slot.bookText}
           </li>
         );
       })}
@@ -41,35 +71,30 @@ const Court = () => {
 const BookSlotContainer = () => {
   return (
     <>
+    <IconContext.Provider value={{ size: "2rem" }}>
       <div>
         <img src={badminton} className="img-wrap" />
       </div>
 
-      <div className="bookslot-container pt-3 pb-3 bg-theme-orange">
-        <div className="d-flex flex-row jc-center">
-          <h3>Time Slots</h3>
-        </div>
-        <div className="court-slots">
-          <div className="d-flex flex-row jc-center">
-            <IconContext.Provider
-              value={{ size:'2rem' }}
-            >
-              <FaIcons.FaChevronCircleLeft />
-              <h3 className="ml-20 mr-20">Court 1/3</h3>
-              <FaIcons.FaChevronCircleRight />
-            </IconContext.Provider>
-          </div>
-        </div>
-      </div>
-
       <div className="bookslot-container">
         <div className="time-slots">
+          <div className="h-40 pt-3 pb-3 bg-theme-orange">
+          <FaIcons.FaCalendar />
+          </div>
           <TimeSlots />
         </div>
         <div className="court-slots">
+          <div className="h-40  pt-3 pb-3 d-flex flex-row jc-center bg-theme-orange">
+            
+              <FaIcons.FaChevronCircleLeft />
+              <h3 className="ml-20 mr-20">Court 1/3</h3>
+              <FaIcons.FaChevronCircleRight />
+           
+          </div>
           <Court />
         </div>
       </div>
+      </IconContext.Provider>
     </>
   );
 };
