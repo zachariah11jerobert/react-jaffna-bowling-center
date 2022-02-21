632a82af52327d69f6ea44b920a1d04de26feafb
unknown <zachariah11jerobert@gmail.com>
2022-02-21 06:31:18 +0530

    done chnages

--- src/components/BookSlotContainer/BookSlotContainer.js
+++ src/components/BookSlotContainer/BookSlotContainer.js
@@ -1,4 +1,5 @@
 import "./bookslotcontainer.scss";
+import badminton from "../../assets/badminton.jpg";
 
 function pad(num) {
   num = num.toString();
@@ -37,16 +38,33 @@ const Court = () => {
 
 const BookSlotContainer = () => {
   return (
-    <div className="bookslot-container">
-      <div className="time-slots">
-        <div>xxx</div>
-        <TimeSlots />
+    <>
+      <div>
+        <img src={badminton} className="img-wrap" />
       </div>
-      <div className="court-slots">
-        <div>Court 1/3</div>
-        <Court />
+
+      <div className="bookslot-container pt-3 pb-3 bg-theme-orange">
+        <div className="time-slots">
+          <h3>Time Slots</h3>
+        </div>
+        <div className="court-slots">
+          <div className="d-flex flex-row jc-spacearound">
+            <span>{"<"}</span>
+            <h3>Court 1/3</h3>
+            <span>{">"}</span>
+          </div>
+        </div>
       </div>
-    </div>
+
+      <div className="bookslot-container">
+        <div className="time-slots">
+          <TimeSlots />
+        </div>
+        <div className="court-slots">
+          <Court />
+        </div>
+      </div>
+    </>
   );
 };
 
